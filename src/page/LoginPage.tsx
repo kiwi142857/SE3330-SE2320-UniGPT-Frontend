7e03509919358f434f04e589170fe2778aa0e374
kiwi142857 <779490624@qq.com>
2024-04-21 21:18:11 +0800

    add service.common.ts

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -3,10 +3,12 @@ import Navigator from '../components/Navigator';
 import '../css/LoginInPage.css';
 
 const LoginPage: React.FC = () => {
+
+    const loginRequestUrl = "http://jaccount.sjtu.edu.cn/oauth2/authorize?response_type=code&scope=profile&client_id=ov3SLrO4HyZSELxcHiqS&redirect_uri=http://localhost:3000/login";
     return (
         <div>
             <Navigator></Navigator>
-            <a href="https://i.sjtu.edu.cn/jaccountlogin" className="video-left-link"></a>
+            <a href={loginRequestUrl} className="video-left-link"></a>
             <video className='home-bg' autoPlay loop muted>
                 <source src="/assets/loginpage.mp4" type="video/mp4" />
             </video>
---
+++ src/service/common.ts
@@ -0,0 +1,62 @@
+type FetchOptions = {
+    method: string;
+    body?: string;
+    headers?: {
+        'Content-Type': string;
+    };
+    credentials: RequestCredentials;
+};
+
+interface ResponseData {
+    ok: boolean;
+    message: string;
+}
+
+export async function getJson(url: string): Promise<any> {
+    let res = await fetch(url, { method: "GET", credentials: "include" });
+    return res.json();
+}
+
+export async function get(url: string): Promise<Response> {
+    let res = await fetch(url, { method: "GET", credentials: "include" });
+    return res;
+}
+
+export async function put(url: string, data: any): Promise<any> {
+    let opts: FetchOptions = {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: "include"
+    };
+    let res = await fetch(url, opts);
+    return res.json();
+}
+
+export async function del(url: string, data: any): Promise<any> {
+    let res = await fetch(url, { method: "DELETE", credentials: "include", body: JSON.stringify(data) });
+    return res.json();
+}
+
+export async function post(url: string, data: any): Promise<any> {
+    let opts: FetchOptions = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: "include"
+    };
+    let res = await fetch(url, opts);
+    return res.json();
+}
+
+export const BASEURL: string = process.env.REACT_APP_BASE_URL ?? 'http://localhost:8080';
+export const PREFIX: string = `${BASEURL}/api`;
+export const API_DOCS_URL: string = `${BASEURL}/api-docs`;
+export const DUMMY_RESPONSE: ResponseData = {
+    ok: false,
+    message: "网络错误！"
+}
