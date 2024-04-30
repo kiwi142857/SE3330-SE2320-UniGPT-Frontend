227d8466acbba4c8a934233d822b7d8fc05ddd3e
wdl339 <2720609228@qq.com>
2024-04-30 10:03:20 +0800

    comments show

--- src/page/BotDetailPage.tsx
+++ src/page/BotDetailPage.tsx
@@ -7,18 +7,16 @@ import BotCarousel from '../components/BotCarousel';
 import BotDetailCard from '../components/BotDetailCard';
 import { CommentInput } from '../components/Inputs';
 import OneChat from '../components/OneChat';
-import { Comment, botDetailInfo, getBotComments, getBotDetail } from '../service/BotDetail';
+import { Comment, CommentList, botDetailInfo, getBotComments, getBotDetail, postComment } from '../service/BotDetail';
 
 import '../css/App.css';
 import '../css/BotDetailPage.css';
-
-// 评论
 
 
 // bot详情页
 const BotDetailPage: React.FC = () => {
     const [bot, setBot] = useState<botDetailInfo | null>(null);
-    const [comments, setComments] = useState<Comment[] | null>(null);
+    const [comments, setComments] = useState<CommentList>();
 
     let { id } = useParams<{ id: string }>();
 
@@ -32,8 +30,8 @@ const BotDetailPage: React.FC = () => {
 
     const getComments = async () => {
         if (id) {
-            const comments = await getBotComments(id);
-            setComments(comments.items);
+            let com = await getBotComments(id, 0, 20);
+            setComments(com);
         }
     }
 
@@ -42,52 +40,45 @@ const BotDetailPage: React.FC = () => {
         getComments();
     }, [id]);
 
-    return [
+    return [  bot && comments &&
         <div className="main-container bot-detail-container">
 
             <BotDetailCard
-                id={bot?.id || ''}
-                name={bot?.name || ''}
-                author={bot?.creator || ''}
-                avatar={bot?.avatar || ''}
-                description={bot?.description || ''}
-                likeNumber={bot ? bot.likeNumber.toString() : ''}
-                starNumber={bot ? bot.starNumber.toString() : ''}
-                isLiked={bot?.liked || false}
-                isStarred={bot?.starred || false}
-                isCreator={bot?.asCreator || false}
+                id={bot.id || ''}
+                name={bot.name || ''}
+                author={bot.creator || ''}
+                avatar={bot.avatar || ''}
+                description={bot.description || ''}
+                likeNumber={bot.likeNumber.toString()}
+                starNumber={bot.starNumber.toString()}
+                isLiked={bot.liked || false}
+                isStarred={bot.starred || false}
+                isCreator={bot.asCreator || false}
             />
 
-            <BotCarousel photos={bot?.photos || []} />
+            <BotCarousel photos={bot.photos || []} />
 
             <Typography
                 sx={{ color: 'primary.light' }}
                 align='left'
             >
                 <p className='bot-detail-long'>
-                    {bot?.detail}
+                    {bot.detail}
                 </p>
             </Typography>
 
             <CommentInput onSend={
                 (content: string) => {
-                    setComments([
-                        {
-                            id: '123',
-                            name: 'AAA 互联网应用 bug 维修',
-                            avatar: '/assets/user-default.png',
-                            content: content
-                        },
-                        ...comments || []
-                    ]);
+                    if (id)
+                        postComment(id,content);
                 }
             } />
 
             <Box>
-                {comments?.map((comment) => (
+                {comments.comments?.map((comment : Comment) => (
                     <OneChat
                         id={comment.id}
-                        name={comment.name}
+                        name={comment.userName}
                         avatar={comment.avatar}
                         content={comment.content}
                     />
