4e7e26f45fdc3e1244fc8838479024fb0f3b02e6
kiwi142857 <779490624@qq.com>
2024-05-05 16:43:25 +0800

    增加market页的分页按钮，总页数需要增加接口,暂定10

--- src/page/MarketPage.tsx
+++ src/page/MarketPage.tsx
@@ -7,14 +7,16 @@ import { getSearchBotList } from '../service/bot';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { BotList, BotListType } from '../components/BotList';
+import { Pagination } from '@mui/material';
+import '../css/Market.css';
 // bot市场
 const MarketPage: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const pageIndexStr = searchParams.get("pageIndex");
     const pageSizeStr = searchParams.get("pageSize");
-    const pageIndex = pageIndexStr != null ? Number.parseInt(pageIndexStr) : 0;
-    const pageSize = pageSizeStr != null ? Number.parseInt(pageSizeStr) : 50;
+    const [pageIndex, setPageIndex] = useState(pageIndexStr != null ? Number.parseInt(pageIndexStr) -1  : 0);
+    const pageSize = pageSizeStr != null ? Number.parseInt(pageSizeStr) : 15;
 
     const [bots, setBots] = useState([]); // [botListType
     const getSearchBots = async () => {
@@ -44,6 +46,12 @@ const MarketPage: React.FC = () => {
         setSearchParams({ keyword: keyword });
     }
 
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        console.log("page change", value -1);
+        setPageIndex(value - 1);
+        setSearchParams({ pageIndex: (value).toString() }); // Update the pageIndex when the page changes
+    };
+
     const botListType: BotListType = {
         type: 'Market'
     };
@@ -55,6 +63,9 @@ const MarketPage: React.FC = () => {
             </div>
             <div style={{ marginTop: '20px' }} className='market-card'>
                 <BotList type={botListType} bots={bots}></BotList>
+            </div>
+            <div style={{ marginTop: '20px',display: 'flex', justifyContent: 'center'}}>
+                <Pagination count={10} page={pageIndex +1 } onChange={handlePageChange} className='pagination'/> 
             </div>
         </div>
     );
