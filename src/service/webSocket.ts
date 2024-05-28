c52bd8970558aa77c9f8b51a88e38fee79a76b1a
kiwi142857 <779490624@qq.com>
2024-05-28 21:04:29 +0800

    with error

--- src/service/webSocket.ts
+++ src/service/webSocket.ts
@@ -1,27 +1,35 @@
 import { websocketUrl } from "./common";
 
 export const createWebSocketConnection = (id: number) => {
-    // Create a new WebSocket connection
-    const socket = new WebSocket(websocketUrl);
+    return new Promise<WebSocket>((resolve, reject) => {
+        // Create a new WebSocket connection
+        const socket = new WebSocket(websocketUrl);
 
-    // Send a message when the connection is open
-    socket.onopen = () => {
-        const message = JSON.stringify({
-            historyId: id.toString(),
-        });
-        console.log("WebSocketConnection onopen: ", message);
-        socket.send(message);
+        // Send a message when the connection is open
+        socket.onopen = () => {
+            const message = JSON.stringify({
+                historyId: id.toString(),
+            });
+            console.log("WebSocketConnection onopen: ", message);
+            socket.send(message);
 
-    };
+            // Resolve the promise with the socket
+            resolve(socket);
+        };
 
-    return socket;
+        // Reject the promise if there is an error
+        socket.onerror = (error) => {
+            reject(error);
+        };
+    });
 };
 
-export const sendMessage = (socket: WebSocket | null, message: string, cover: boolean = false) => {
+export const sendMessage = (socket: WebSocket | null, message: string, cover: boolean = false, userAsk:boolean = false) => {
     if (socket) {
         const sendContent = JSON.stringify({
             chatContent: message,
-            cover: cover
+            cover: cover,
+            userAsk:userAsk
         });
         console.log("sendMessage: ", message);
         socket.send(sendContent);
