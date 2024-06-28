949cbb7bd352a67598694ca93b146e6180ea34a0
wdl339 <2720609228@qq.com>
2024-06-28 16:23:54 +0800

    appRouter配置PluginDetailPage
    Signed-off-by: wdl339 <2720609228@qq.com>

---
+++ src/page/PluginDetailPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PluginDetailPage = () => {
+    return (
+        <div style={{ marginTop: '200px' }}>
+            <h1>{"Plugin Detail Page"}</h1>
+        </div>
+    );
+}
+
+export default PluginDetailPage;
